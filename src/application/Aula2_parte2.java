5c68ea0d9fbb570761f07ba07f4995f6390e7a14
jnorgini <jnorgini@gmail.com>
2022-12-15 13:36:00 -0300

    aula 2 - cálculo lambda

---
+++ src/application/Aula2_parte2.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
+
+public class Aula2_parte2 {
+	
+	public static int compareProducts(Product p1, Product p2) {
+		return p1.getPrice().compareTo(p2.getPrice());
+	}
+
+	public static void main(String[] args) {
+		
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Notebook", 1200.00));
+		list.add(new Product("Tablet", 450.00));
+		
+		list.sort(Aula2_parte2::compareProducts);
+		
+		list.forEach(System.out::println);
+
+	}
+
+}
